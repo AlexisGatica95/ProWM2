c856a9bd42ebacd5b81c1cabeebe0d8bdd9c0113
alexio <alexisgatica95@gmail.com>
2019-10-01 22:48:53 -0300

    lpm

--- Clase4/proyectoTarea/proyecto1/models/reservasModel.js
+++
@@ -1,9 +0,0 @@
-function reservasFn(reservas) {
-    
-    console.log('el modelo funciona');
-    return {status: 'pito', nombre: 'alexio'};
-    
-
-}
-
-module.exports = {reservasFn};
--- Clase4/proyectoTarea/proyecto1/models/usuariosModel.js
+++
@@ -1,9 +0,0 @@
-function usuariosFn(usuarios) {
-    
-    console.log('el modelo funciona');
-    return {status: 'pito', nombre: 'alexio'};
-    
-
-}
-
-module.exports = {usuariosFn};
